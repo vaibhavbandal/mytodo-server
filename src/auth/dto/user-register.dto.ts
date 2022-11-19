ef61e751de83115ae5c0685b209b6989dfddc4e8
Shreyash Kolhe <90944900+shreyash510@users.noreply.github.com>
2022-11-19 12:29:13 +0530

    remove swagger

--- src/auth/dto/user-register.dto.ts
+++ src/auth/dto/user-register.dto.ts
@@ -1,14 +1,11 @@
 import { Role } from "@prisma/client"
 import {IsEmail, IsString} from 'class-validator'
-import {ApiProperty} from '@nestjs/swagger'
 
 export class RegisterUserDto {
     @IsEmail()
-    @ApiProperty()
     email : string
     
     @IsString()
-    @ApiProperty()
     password : string
 
 }
