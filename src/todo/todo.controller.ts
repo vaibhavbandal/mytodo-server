68f491b224a3fe40a008621719e51bf99c840f15
Shreyash Kolhe <90944900+shreyash510@users.noreply.github.com>
2022-11-26 23:07:03 +0530

    todo Apis done

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,35 +1,41 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards, Request } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { JwtAuthGuard } from 'src/auth/utils/Guards';
 import { AddTodoDto } from './dto/add-todo.dto';
 import { UpdateTodoImportantDto } from './dto/update-todo-important.dto';
 import { UpdateTodoStatusDto } from './dto/update-todo-status.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
 import { TodoService } from './todo.service';
 
+
+@UseGuards(JwtAuthGuard)
 @Controller('todo')
 export class TodoController {
   constructor(private readonly todoService: TodoService) { }
 
   @Get('getall')
-  async getAllTodos() {
-    return this.todoService.getAllTodos();
+  async getAllTodos(@Request() req: any) {
+    const user = req.user;
+    return this.todoService.getAllTodos(user);
   }
 
   @Get('getbyid/:id')
-  async getById(@Param('todoId') todoId: number) {
+  async getById(@Param('id') todoId: string) {
     return this.todoService.getById(todoId);
   }
 
   @Post('add')
-  async addTodo(@Body() bodyData: AddTodoDto) {
-    return this.todoService.addTodo(bodyData);
+  async addTodo(@Body() bodyData: AddTodoDto, @Request() req: any) {
+    const user = req.user;
+    return this.todoService.addTodo(bodyData, user);
   }
 
   @Delete('deletebyid/:id')
-  async deleteByTodo(@Param('todoId') todoId: number) {
+  async deleteByTodo(@Param('id') todoId: string) {
     return this.todoService.deleteByTodo(todoId);
   }
 
-  @Put('updatebyid/:id')
+  @Put('updatebyid')
   async updateTodo(@Body() bodyData: UpdateTodoDto) {
     return this.todoService.updateTodo(bodyData);
   }
@@ -38,5 +44,16 @@ export class TodoController {
   async updateTodoStatus(@Body() bodyData: UpdateTodoStatusDto) {
     return this.todoService.updateTodoStatus(bodyData);
   }
+
+  @Put('isImportant')
+  async isImportantUpdate(@Body() bodyData : UpdateTodoImportantDto){
+    return this.todoService.updateTodoImportant(bodyData);
+  }
+
+  @Get('getIsImportant')
+  async getIsImportant(){
+    return this.todoService.getIsImportant();
+  }
+
 
 }
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,42 +1,146 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { AddTodoDto } from './dto/add-todo.dto';
+import { UpdateTodoImportantDto } from './dto/update-todo-important.dto';
 import { UpdateTodoStatusDto } from './dto/update-todo-status.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
 
 @Injectable()
 export class TodoService {
+  constructor(
+    private readonly prismaService: PrismaService,
+  ) { }
 
-async getAllTodos(){
-  return 'all Todos';
-}
+  async getAllTodos(user: any) {
+    try {
+      const allTodos = await this.prismaService.todo.findMany({
+        where: {
+          userId: user.userId
+        }
+      });
+      return allTodos
+    } catch (error) {
+      throw new HttpException(
+        { msg: 'Not found' },
+        HttpStatus.FORBIDDEN,
+      );
+    }
+  }
 
-async getById(getById : number){
-  return 'todo by id';
-}
+  async getById(todoId: string) {
+    try {
+      const getTodoById = await this.prismaService.todo.findUnique({
+        where: { id: todoId }
+      });
+      return getTodoById;
+    } catch (error) {
+      throw new HttpException(
+        { msg: 'Not found' },
+        HttpStatus.FORBIDDEN,
+      );
+    }
+  }
 
-async addTodo(todoBody : AddTodoDto){
-  return 'Add todo sucessfully';
-}
+  async addTodo(todoBody: AddTodoDto, user: any) {
+    try {
+      const todo = await this.prismaService.todo.create({
+        data: {
+          userId: user.userId,
+          title: todoBody.title,
+          desc: todoBody.desc,
+          isImportant: todoBody.isImportant,
+        }
+      })
+      return todo
+    } catch (error) {
+      throw new HttpException(
+        { msg: 'Try again!' },
+        HttpStatus.FORBIDDEN,
+      );
+    }
+  }
 
-async deleteByTodo(todoId : number){
-  return 'delete todo'
-}
+  async deleteByTodo(todoId: string) {
+    try {
+      const todo = await this.prismaService.todo.delete({
+        where: {
+          id: todoId
+        }
+      })
+      return todo
+    } catch (error) {
+      throw new HttpException(
+        { msg: 'Try again!' },
+        HttpStatus.FORBIDDEN,
+      );
+    }
+  }
 
-async updateTodo(bodyData : UpdateTodoDto){
-  return 'update todo'
-}
+  async updateTodo(bodyData: UpdateTodoDto) {
+    const todo = await this.prismaService.todo.update({
+      data: {
+        title: bodyData.title,
+        desc: bodyData.desc
+      },
+      where: {
+        id: bodyData.todoId
+      }
+    })
+    return todo
+  }
 
-async updateTodoStatus(bodyData: UpdateTodoStatusDto){
-  return 'body status';
-}
+  async updateTodoStatus(bodyData: UpdateTodoStatusDto) {
+    try {
+      const todo = await this.prismaService.todo.update({
+        data: {
+          status: bodyData.status
+        },
+        where: {
+          id: bodyData.todoId
+        }
+      })
+      return todo
+    } catch (error) {
+      throw new HttpException(
+        { msg: 'Try again!' },
+        HttpStatus.FORBIDDEN,
+      );
+    }
+  }
 
-  // async updateTodoStatus(bodyData: UpdateTodoStatusDto) {
-  //   return bodyData;
-  // }
-  // async updateTodoImportant(bodyData: UpdateTodoImportantDto) {
-  //   return bodyData;
-  // }
-  // async getImportantTodo() {
-  //   return 'important-todo';
-  // }
+  async updateTodoImportant(bodyData: UpdateTodoImportantDto) {
+    try {
+      const todo = await this.prismaService.todo.update({
+        data: {
+          isImportant: bodyData.isImportant
+        },
+        where: {
+          id: bodyData.todoId
+        }
+      })
+      return todo;
+    } catch (error) {
+      throw new HttpException(
+        { msg: 'Try again!' },
+        HttpStatus.FORBIDDEN,
+      );
+    }
+  }
+
+  async getIsImportant() {
+    try {
+      const getIsImportantTodos = await this.prismaService.todo.findMany({
+        where: {
+          isImportant: true
+        }
+      })
+      return getIsImportantTodos
+    } catch (error) {
+      throw new HttpException(
+        { msg: 'Try again!' },
+        HttpStatus.FORBIDDEN,
+      );
+    }
+  }
 }
